706d2f103831810a5821d0ba163582a76edaf87b
bbishop19 <bbishop19@windwardschool.org>
2018-12-08 18:05:28 -0800

    redo logic

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application {
         // first window
         Group root = new Group();
         Button temp = new Button();
-        temp.setText("Click for a new session. Check box for VIP.");
+        temp.setText("Please Select a Seat. ");
 
         CheckBox VIPCheck = new CheckBox(); // for VIP
         VIPCheck.setTranslateX(325);
@@ -35,7 +35,7 @@ public class MainApplication extends Application {
         root.getChildren().add(VIPCheck);
 
         // show first window
-        primaryStage.setTitle("App");
+        primaryStage.setTitle("Malicious Virus");
         primaryStage.setScene(new Scene(root,  350, temp.getPrefHeight(), Color.GAINSBORO));
         primaryStage.show();
 
@@ -53,73 +53,5 @@ public class MainApplication extends Application {
         Scene checkOut = new Scene(cRoot, 1250, 675);
 
         //FIXME see what else we can move out to make it less laggy
-        temp.setOnAction(new EventHandler<ActionEvent>(){ // https://stackoverflow.com/questions/15041760/javafx-open-new-window
-            @Override
-            public void handle(ActionEvent event){
-
-                // make this session and determine whether or not it's VIP
-                Session thisSession = new Session(manager);
-                thisSession.setIsVIP(VIPCheck.isSelected());
-
-                // track this session so we can communicate with it later
-                Text ID = new Text("" + manager.add(thisSession));
-                ID.setVisible(false);
-                ID.setFont(Font.font(0));
-
-
-                GridPane gp = new GridPane(); //FIXME it gets very angry if we move this outside this eventhandler
-
-                // sets up scene with the table but not shown yet
-                // FIXME: since the reservation thing sounds intentional, maybe we should include a text thing on the screen that says click to reserve or smth
-                stage.setScene(new Scene(gp, 1250, 675));
-
-                // setting up the table of seats as buttons
-                for (int i = 0; i < manager.getSeats().length; i++){
-                    for (int j = 0; j<manager.getSeats()[i].length; j++){
-                        Button newSeat = new Button();
-                        newSeat.setTextOverrun(OverrunStyle.CLIP);
-                        newSeat.setText("" + manager.getSeats()[i][j].getRow() + manager.getSeats()[i][j].getColumn());
-                        newSeat.setPrefSize(50, 25);
-
-                        // if the seat is available
-                        if (manager.getSeats()[i][j].getAvailable()) {
-                            newSeat.setStyle("-fx-background-color: MediumSeaGreen");
-                            gp.add(newSeat, manager.getSeats()[i][j].getColumn(), manager.getSeats()[i][j].getRowNumericalRepresentation());
-
-                            // any seat will take you to a checkout window and automatically reserve it
-                            newSeat.setOnAction(new EventHandler<ActionEvent>() {
-                                @Override
-                                public void handle(ActionEvent event) {
-                                    try {
-                                        stage.setScene(checkOut);
-                                        manager.getSession(Integer.valueOf(ID.getText())).setSeat(manager.getSeats()[(int)newSeat.getLayoutX()/50][(int)newSeat.getLayoutY()/25]);
-                                        manager.getSession(Integer.valueOf(ID.getText())).startReservation();
-
-                                        //TODO allow for checkout and cancellation. make sure requests for these two are done through the queue
-                                        //TODO if someone ahead of you in queue has already reserved the seat tell them the seat is reserved and boot them
-                                        //TODO change button color depending on reserved status/whatnot
-                                    }
-                                    catch (Exception e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                        }
-                        // if the seat isn't available
-                        else {
-                            newSeat.setStyle("-fx-background-color: DarkRed");
-                            gp.add(newSeat, manager.getSeats()[i][j].getColumn(), manager.getSeats()[i][j].getRowNumericalRepresentation());
-                        }
-                    }
-                }
-
-                // tracks the session on the second window for communication
-                gp.add(ID, 0, manager.getSeats().length);
-                gp.setAlignment(Pos.CENTER);
-
-                // show the second window
-                stage.show();
-            }
-        });
     }
 }
